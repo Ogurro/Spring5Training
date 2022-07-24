33cbd2e9f41df7ca60a168df59281687d7804fe7
Pawel Wilkolek <ogur.pawel@gmail.com>
2022-07-24 23:19:13 +0200

    Publisher Entity basic structure and Repository

---
+++ src/main/java/com/pw/spring5training/model/Publisher.java
@@ -0,0 +1,62 @@
+package com.pw.spring5training.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String zip;
+
+    public Publisher(String name, String addressLine1, String city, String state, String zip) {
+        this.name = name;
+        this.addressLine1 = addressLine1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Publisher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                (addressLine1 == null ? "" : (", addressLine1='" + addressLine1 + '\'')) +
+                (city == null ? "" : (", city='" + city + '\'')) +
+                (state == null ? "" : (", state='" + state + '\'')) +
+                (zip == null ? "" : (", zip='" + zip + '\'')) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Publisher publisher = (Publisher) o;
+
+        return Objects.equals(id, publisher.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
