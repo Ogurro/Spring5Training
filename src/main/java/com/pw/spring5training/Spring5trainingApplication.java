6b1266ecf2a60908d989a13df7c5558582497ce8
Pawel Wilkolek <ogur.pawel@gmail.com>
2022-07-24 20:38:30 +0200

    init commit

---
+++ src/main/java/com/pw/spring5training/Spring5trainingApplication.java
@@ -0,0 +1,13 @@
+package com.pw.spring5training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5trainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5trainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pw/spring5training/Spring5trainingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw.spring5training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5trainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
