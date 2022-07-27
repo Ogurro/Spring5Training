a270f335ad9cfae2eb28805ff7dbfcf9f8883a8f
Pawel Wilkolek <ogur.pawel@gmail.com>
2022-07-28 00:33:58 +0200

    qualifiers dependency injection #2

---
+++ src/main/java/com/pw/spring5training/controller/dependencyInjection/PrimaryInjectedController.java
@@ -0,0 +1,19 @@
+package com.pw.spring5training.controller.dependencyInjection;
+
+import com.pw.spring5training.services.GreetingService;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PrimaryInjectedController {
+
+    private final GreetingService greetingService;
+
+    public PrimaryInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
+
+}
---
+++ src/test/java/com/pw/spring5training/controller/dependencyInjection/PrimaryInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package com.pw.spring5training.controller.dependencyInjection;
+
+import com.pw.spring5training.services.PrimaryGreetingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimaryInjectedControllerTest {
+
+    PrimaryInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new PrimaryInjectedController(new PrimaryGreetingService());
+    }
+
+    @Test
+    void getGreeting() {
+        assertEquals("Hello World - Primary", controller.getGreeting());
+    }
+}
